6e4465282a359619aca665faa1b30998909c7840
Louie D <louie.delrosario97@gmail.com>
2026-04-14 12:43:31 -0700

    Fixed the date scan by creating a DateTimeFormatter and a LocalDate.parse

--- src/com/pluralsight/TheaterReservations.java
+++ src/com/pluralsight/TheaterReservations.java
@@ -1,24 +1,26 @@
 package com.pluralsight;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class TheaterReservations {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter Full Name: ");
-        String fullName = scan.nextLine();
+        Scanner scan = new Scanner(System.in); // Add Scanner
+        System.out.print("Please enter your name: "); // Prompt for name
+        String fullName = scan.nextLine(); // Scan for prompt above
 
-        System.out.print("Date of Show (MM/DD/YYYY): ");
-        String dateOfShow = scan.nextLine();
-        DateTimeFormatter DateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        System.out.print("What date will you be coming (MM/DD/YYYY): "); //Prompt for date
+        String dateOfShow = scan.nextLine(); // Scan for prompt above
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy"); //Use 'DateTimeFormatter'
+        LocalDate reservationDate = LocalDate.parse(dateOfShow, formatter);
 
-        System.out.print("Enter # of Tickets: ");
+        System.out.print("How many tickets would you like? : ");
         int numberOfTickets = scan.nextInt();
 
         if (numberOfTickets > 1) {
-            System.out.println("You have " + numberOfTickets + " tickets reserved for " + DateFormat + " under " + fullName + ".");
+            System.out.println("You have " + numberOfTickets + " tickets reserved for " + reservationDate + " under " + fullName + ".");
         } else {
-            System.out.println("You have " + numberOfTickets + " ticket reserved for " + DateFormat + " under " + fullName + ".");
+            System.out.println("You have " + numberOfTickets + " ticket reserved for " + reservationDate + " under " + fullName + ".");
         }
     }
 }
